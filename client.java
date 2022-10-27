17d6db293f58a315b832721a2bf5482fea86fa7f
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-10-27 14:22:18 +0100

    lixo

--- client.java
+++ client.java
@@ -1,31 +1,27 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 
-public class oNode {
-
-    public void server() {
+public class client {
+    public void cliente() {
         try {
-            ServerSocket server = new ServerSocket(5000);
-            Socket socket = server.accept();
+            Socket socket = new Socket("10.0.0.10", 5000);
             DataInputStream in = new DataInputStream(socket.getInputStream());
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            String str = in.readUTF();
-            System.out.println("Client: " + str);
-            out.writeUTF("Hello Client");
+            out.writeUTF("Hello Server");
             out.flush();
+            String str = in.readUTF();
+            System.out.println("Server: " + str);
             out.close();
             in.close();
             socket.close();
-            server.close();
         } catch (Exception e) {
             System.out.println(e);
         }
     }
 
     public static void main(String[] args) {
-        oNode node = new oNode();
-        node.server();
+        client client = new client();
+        client.cliente();
     }
 }
