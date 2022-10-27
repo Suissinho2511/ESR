534a401582cb2591329d2546ba51c3a05cdacc9a
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-10-27 14:49:31 +0100

    yesh

---
+++ server.java
@@ -0,0 +1,30 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public Server() {
+        try {
+            ServerSocket server = new ServerSocket(5000);
+            Socket socket = server.accept();
+            // DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            // String str = in.readUTF();
+            System.out.println("Client: " + str);
+            out.writeUTF("Hello Client");
+            out.flush();
+            out.close();
+            in.close();
+            socket.close();
+            server.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+
+}
