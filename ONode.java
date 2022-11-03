edc8a75f9471095b322850f96cbbc7152ae4f31b
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-11-03 14:54:08 +0000

    Update ONode.java

--- ONode.java
+++ ONode.java
@@ -1,14 +1,16 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ONode {
-    public ONode(String ip1, String ip2) {
+    public ONode(String ip) {
         try {
-            try (Socket socketR = new Socket(ip1, 5000)) {
-                try (Socket socketS = new Socket(ip2, 5000)) {
-                    DataInputStream in = new DataInputStream(socketR.getInputStream());
-                    DataOutputStream out = new DataOutputStream(socketS.getOutputStream());
+            try (ServerSocket server = new ServerSocket(5000)) {
+                Socket socket = server.accept();
+                try (Socket socketS = new Socket(ip, 5000)) {
+                    DataInputStream in = new DataInputStream(socketS.getInputStream());
+                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                     while (true) {
                         String str = in.readUTF();
                         System.out.println("Server: " + str);
@@ -23,6 +25,6 @@ public class ONode {
     }
 
     public static void main(String[] args) {
-        ONode oNode = new ONode(args[1], args[2]);
+        ONode oNode = new ONode(args[1]);
     }
 }
