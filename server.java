53357541ceb0dd41e7fefec2db1e502f7d9c9c2e
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-11-03 15:21:19 +0000

    b

--- server.java
+++ server.java
@@ -6,15 +6,15 @@ import java.net.Socket;
 public class Server {
     public Server(String ip) {
         try {
-            ServerSocket serverR = new ServerSocket(5000);
-            Socket socketR = serverR.accept();
+            // ServerSocket serverR = new ServerSocket(5000);
+            // Socket socketR = serverR.accept();
             Socket socketS = new Socket(ip, 5000);
-            DataInputStream in = new DataInputStream(socketR.getInputStream());
+            // DataInputStream in = new DataInputStream(socketR.getInputStream());
             DataOutputStream out = new DataOutputStream(socketS.getOutputStream());
             int i = 0;
             while (true) {
-                String str = in.readUTF();
-                System.out.println("Server: " + str);
+                // String str = in.readUTF();
+                // System.out.println("Server: " + str);
                 out.writeUTF("Hello client: " + i++);
                 out.flush();
             }
