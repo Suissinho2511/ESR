c5b111a8d4101d257388ddd3ef9e687127e633e7
Vasco Mota de Oliveira <56849988+Suissinho2511@users.noreply.github.com>
2022-11-07 13:08:45 +0000

    <>

--- UDP/oNode.java
+++ UDP/oNode.java
@@ -21,7 +21,7 @@ public class oNode {
             while (true) {
                 socket_in.receive(packet);
                 String str = new String(packet.getData(), 0, packet.getLength());
-                str = "+++ " + str + " +++";
+                str = "< " + str + " >";
                 buffer = str.getBytes();
                 for (String ip : neighborsIP) {
                     packet = new DatagramPacket(buffer, buffer.length, InetAddress.getByName(ip), 5000);
