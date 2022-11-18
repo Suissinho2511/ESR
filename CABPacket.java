d63991ae0c8cfc5a136be183e9be649f68f2f481
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2022-11-18 12:02:43 +0000

    Classe específica para pacote de controlo

--- CABPacket.java
+++ CABPacket.java
@@ -1,61 +1,17 @@
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.net.DatagramPacket;
-import java.net.InetAddress;
-import java.util.LinkedHashMap;
-import java.util.Map.Entry;
 
-public class CABPacket implements Serializable{
+public abstract class CABPacket implements Serializable {
 
-    private String type;
-    private int availableJumps;
-
-    private LinkedHashMap<InetAddress, Long> path;
-    
+    // -------------------------------------
+    // Confusing And Bloated protocol Packet
+    // -------------------------------------
 
 
-    public CABPacket(String type, int maxJumps, InetAddress currAddress, Long timestamp) {
-        this.type = type;
-        this.availableJumps = maxJumps;
-        this.path = new LinkedHashMap<>();
-        this.path.put(currAddress, timestamp);
-    }
-
-    public CABPacket(DatagramPacket packet, InetAddress currAddress, Long timestamp){
-        byte[] payload = packet.getData();
-
-
-    }
-    
-
-    public <K, V> Entry<InetAddress, Long> getFirst() {
-        if (this.path.isEmpty()) return null;
-        return this.path.entrySet().iterator().next();
-    }
-
-    public <K, V> Entry<InetAddress, Long> getLast() throws NoSuchFieldException, IllegalAccessException{
-        Field tail = this.path.getClass().getDeclaredField("tail");
-        tail.setAccessible(true);
-        return (Entry<InetAddress, Long>) tail.get(this.path);
-      }
-
-
-
-    public int getAvailableJumps() {
-        return this.availableJumps;
-    }
-
-    public void addNode(InetAddress ip) {
-        path.put(ip, System.currentTimeMillis());
-        availableJumps--;
-    }
-
-    public long getDelay() {
-        return System.currentTimeMillis() - this.getFirst().getValue();
-    }
-
-    public String[] getPath() {
-        return path.keySet().stream().map(InetAddress::getHostAddress).toArray(String[]::new);
-    }
-
+	/*
+	 * +----------------+----------------------------------------------------------+
+	 * | 	  type		|							...							   |
+	 * +----------------+----------------------------------------------------------+
+	 * 
+	 */
+    public String type;
 }
