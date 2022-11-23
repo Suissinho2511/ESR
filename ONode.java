473168c9a19faefb00555adb5a816e572da90390
a93234@alunos.uminho.pt <DiogoPereiraMatos>
2022-11-23 11:51:09 +0000

    Sugestion. Seria preciso usar TCP para controlo, o que até faz sentido porque não queremos perder pacotes de controlo. E só seria preciso ter connecções com os vizinhos.

--- ONode.java
+++ ONode.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -70,15 +74,18 @@ public class ONode {
 				byte[] ctrl_buffer = new byte[20000];
 				DatagramPacket ctrl_packet = new DatagramPacket(ctrl_buffer, ctrl_buffer.length);
 
-				// TODO: probe (send initial control packet)
+				// Send initial probe request
+				//Graph.writeToSocket(null, topology);
 
 				while (true) {
 					socket_control.receive(ctrl_packet);
 					byte[] data = ctrl_packet.getData();
 
-					// TODO: merge current topology with probed topology (+delays)
-					// TODO: if it's the same do nothing, else:
-					// TODO: build new packet with our new topology
+					// TODO: é preciso usar TCP aqui
+					//Graph other = Graph.readFromSocket(null);
+					//boolean changed = topology.merge(other));
+					//if(!changed) continue;
+					//Graph.writeToSocket(null, topology);
 
 					for (String ip : neighborsIP) {
 						DatagramPacket out_packet = new DatagramPacket(data, data.length, InetAddress.getByName(ip), 5001);
@@ -111,12 +118,18 @@ class Graph implements Serializable {
 
 	//TODO: Adaptar para incluir delays e ter isso em consideração no pathfinding
 
-	/*private class Vertex {
-		String label;
-		Vertex(String label) {
-			this.label = label;
-		}
-	}*/
+	private class Vertex {
+		public final String label;
+		public Vertex(String label) {this.label = label;}
+	}
+
+	private class Edge {
+		public final Vertex from;
+		public final Vertex to;
+		public Float cost;
+		public Edge(Vertex from, Vertex to) {this.from = from; this.to = to;}
+		public Edge(Vertex from, Vertex to, Float cost) {this.from = from; this.to = to; this.cost = cost;}
+	}
 
 	private final Map<String, List<String>> adjVertices = new HashMap<>();
 
@@ -173,7 +186,45 @@ class Graph implements Serializable {
 		return visited.stream().toList();
 	}
 
-	private List<String> getAdjVertices(String label) {
+	public List<String> getAdjVertices(String label) {
 		return adjVertices.getOrDefault(label, null);
+	}
+
+	public Set<String> getNodes() {
+		return adjVertices.keySet();
+	}
+
+	public boolean merge(Graph other) {
+		boolean result = false; //were there changes
+
+		// check nodes first
+		for(String other_node : other.getNodes())
+			if(!this.getNodes().contains(other_node)) {
+				this.addVertex(other_node);
+				result = true;
+			}
+
+		// now check vertices
+		for(String other_node : other.getNodes())
+			for(String adj : other.getAdjVertices(other_node))
+				if(!this.getAdjVertices(other_node).contains(adj)) {
+					this.addEdge(other_node, adj);
+					result = true;
+				}
+
+		return result;
+	}
+
+	public static Graph readFromSocket(Socket s) throws IOException, ClassNotFoundException {
+		ObjectInputStream in = new ObjectInputStream(s.getInputStream());
+		Graph g = (Graph) in.readObject();
+		in.close();
+		return g;
+	}
+
+	public static void writeToSocket(Socket s, Graph g) throws IOException {
+		ObjectOutputStream out = new ObjectOutputStream(s.getOutputStream());
+        out.writeObject(g);
+        out.close();
 	}
 }
