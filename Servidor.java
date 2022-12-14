5cec194fd66f6aa89abc3c23d178cb2f70f712e1
Duarte Serrão <duarte.serrao.1999@gmail.com>
2022-12-14 15:27:18 +0000

    see if it works

--- Servidor.java
+++ Servidor.java
@@ -66,6 +66,7 @@ public class Servidor extends JFrame implements ActionListener {
     try {
       RTPsocket = new DatagramSocket(); // init RTP socket
       ClientIPAddr = InetAddress.getByName(argv);
+      RTPsocket.connect(ClientIPAddr, RTP_dest_port);
       System.out.println("Servidor: socket " + ClientIPAddr);
       video = new VideoStream(VideoFileName); // init the VideoStream object:
       System.out.println("Servidor: vai enviar video da file " + VideoFileName);
